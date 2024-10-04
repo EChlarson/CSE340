d611b429583db2f26de5fdc6241b70f19adb9960
EChlarson <chl20001@byui.edu>
2024-10-04 14:01:49 -0500

    details view cont

--- controllers/detailsController.js
+++ controllers/detailsController.js
@@ -8,16 +8,16 @@ const invDet = {}
  * ************************** */
 invDetails.buildByInventoryId = async function (req, res, next) {
   const inv_id = req.params.invId
-  const data = await invModel.getInventoryByVehicleId(inv_id) //?
-  const grid = await utilities.buildVehicleGrid(data) //?
+  const detailsData = await invModel.getInventoryByVehicleId(inv_id) //?
+  const detailsGrid = await utilities.buildVehicleGrid(detailsData) //?
   let nav = await utilities.getNav()
-  const vehicleMake = data[0].inv_make
-  const vehicleModel = data[0].inv_model
+  const vehicleMake = detailsData[0].inv_make
+  const vehicleModel = detailsData[0].inv_model
 
   res.render("./inventory/details", {
     title: vehicleMake + " " + vehicleModel,
     nav,
-    grid,
+    detailsGrid,
   })
 }
 
